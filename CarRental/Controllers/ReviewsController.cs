82db7addd52a1b93190167324118a39a7b4b7369
natalss <natalia.fotini@gmail.com>
2026-05-21 16:48:23 +0300

    ui changes

--- CarRental/Controllers/ReviewsController.cs
+++ CarRental/Controllers/ReviewsController.cs
@@ -16,35 +16,29 @@ namespace CarRental.Controllers
             _context = context;
             _userManager = userManager;
         }
-
         [HttpPost]
         [Authorize]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddReview(string recipientId, string comment, int rating)
         {
-            try
+            var commenterId = _userManager.GetUserId(User);
+
+            if (string.IsNullOrEmpty(commenterId) || string.IsNullOrEmpty(recipientId))
+                return BadRequest();
+
+            var newReview = new Review
             {
-                var commenterId = _userManager.GetUserId(User);
-                if (string.IsNullOrEmpty(commenterId) || string.IsNullOrEmpty(recipientId))
-                    return Json(new { success = false, message = "Invalid Data" });
+                CommenterId = commenterId,
+                RecipientId = recipientId,
+                Rating = rating,
+                Comment = comment ?? "",
+                IsDeleted = false
+            };
 
-                var newReview = new Review
-                {
-                    CommenterId = commenterId,
-                    RecipientId = recipientId,
-                    Rating = rating,
-                    Comment = comment ?? "",
-                    IsDeleted = false
-                };
+            _context.Reviews.Add(newReview);
+            await _context.SaveChangesAsync();
 
-                _context.Reviews.Add(newReview);
-                await _context.SaveChangesAsync();
-
-                return Json(new { success = true });
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, message = ex.Message });
-            }
+            return RedirectToAction("OpenUsersProfile", "User", new { userId = recipientId });
         }
     }
 }
