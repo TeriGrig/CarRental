49cee5aebf8af4d640ea4c9edf4da6c9505b0456
tereza <terezagrigoraskou@gmail.com>
2026-05-29 19:51:32 +0300

    reports' description added

---
+++ CarRental/Migrations/20260529164901_AddReportDescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Reports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Reports");
+        }
+    }
+}
